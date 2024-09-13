5a2949585a8b615050e9b93d48e6186c8f5637b2
iceice666 <iceice666@outlook.com>
2024-09-14 03:52:09 +0800

    Catch is back!

--- pylox/ast/printer.py
+++ pylox/ast/printer.py
@@ -1,62 +1,64 @@
 from typing import List
 
-from rusty_utils import Err, Result, Ok
+from rusty_utils import Result, Ok, Catch
 
-from pylox.ast.expression import expression, IExpr, Unary, Binary, Grouping, Literal
+from pylox.ast.expression import IExpr, Unary, Binary, Grouping, Literal
 from pylox.lexer.tokens import Token
+from pylox.parser.error import ParseError
+from pylox.parser.expression import expression
 from pylox.parser.source import Source
 
 
-class Resolver:
+class Printer:
 
-    def resolve(self, ast: IExpr, indent: int = 0) -> Result[str, ValueError]:
+    @Catch(ValueError)  # type: ignore
+    def resolve(self, ast: IExpr, indent: int = 0) -> str:
+        res: str
+
         if isinstance(ast, Unary):
-            return self.resolve_unary(ast, indent + 1)
+            res = self.resolve_unary(ast, indent + 1).unwrap_or_raise()
         elif isinstance(ast, Binary):
-            return self.resolve_binary(ast, indent + 1)
+            res = self.resolve_binary(ast, indent + 1).unwrap_or_raise()
         elif isinstance(ast, Grouping):
-            return self.resolve_grouping(ast, indent + 1)
+            res = self.resolve_grouping(ast, indent + 1).unwrap_or_raise()
         elif isinstance(ast, Literal):
-            return self.resolve_literal(ast, indent + 1)
+            res = self.resolve_literal(ast, indent + 1)
         else:
-            return Err(ValueError(f"Invalid ast node: {ast}"))
+            raise (ValueError(f"Invalid ast node: {ast}"))
 
-    def resolve_unary(self, value: Unary, indent: int) -> Result[str, ValueError]:
-        match self.resolve(value.right, indent):
-            case Ok(right):
-                return Ok(f"({value.operator} {right})")
-            case err:
-                return err
+        return res
 
-    def resolve_binary(self, value: Binary, indent: int) -> Result[str, ValueError]:
-        match (self.resolve(value.left, indent), self.resolve(value.right, indent)):
-            case (Ok(left), Ok(right)):
-                return Ok(f"({value.operator}\n"
-                          f"{' ' * indent * 2}{left}\n"
-                          f"{' ' * indent * 2}{right}\n"
-                          f"{' ' * (indent - 1) * 2})")
-            case (err1, err2):
-                return err1.and_(err2)
+    @Catch(ValueError)  # type: ignore
+    def resolve_unary(self, value: Unary, indent: int) -> str:
+        right = self.resolve(value.right, indent).unwrap_or_raise()
+        return f"({value.operator} {right})"
 
-    def resolve_grouping(self, value: Grouping, indent: int) -> Result[str, ValueError]:
-        return self.resolve(value.expression, indent).map(
-            lambda expr: (f"(\n"
-                          f"{' ' * indent * 2}{expr}\n"
-                          f"{' ' * (indent - 1) * 2})")
-        )
+    @Catch(ValueError)  # type: ignore
+    def resolve_binary(self, value: Binary, indent: int) -> str:
+        left = self.resolve(value.left, indent).unwrap_or_raise()
+        right = self.resolve(value.right, indent).unwrap_or_raise()
 
-    def resolve_literal(self, value: Literal, _indent: int) -> Result[str, ValueError]:
-        return Ok(str(value.value))
+        return (f"({value.operator}\n"
+                f"{' ' * indent * 2}{left}\n"
+                f"{' ' * indent * 2}{right}\n"
+                f"{' ' * (indent - 1) * 2})")
+
+    @Catch(ValueError)  # type: ignore
+    def resolve_grouping(self, value: Grouping, indent: int) -> str:
+        expr = self.resolve(value.expression, indent).unwrap_or_raise()
+        return (f"(\n"
+                f"{' ' * indent * 2}{expr}\n"
+                f"{' ' * (indent - 1) * 2})")
+
+    def resolve_literal(self, value: Literal, _indent: int) -> str:
+        return str(value.value)
 
 
-def format_ast(input_: List[Token]) -> str | None:
+def format_ast(input_: List[Token]) -> Result[str, ValueError | ParseError]:
     source = Source(input_)
     ast = expression(source)
-    if ast.is_err():
-        print(ast.unwrap_err())
-        return None
-
-    ast = ast.unwrap()
-    resolved = Resolver().resolve(ast, 0)
-
-    return resolved
+    match ast:
+        case Ok(ast):
+            return Printer().resolve(ast, 0)
+        case err:
+            return err
