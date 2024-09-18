dbb9de6b6b5b933884e80bdbb363bb0e0a138687
iceice666 <iceice666@outlook.com>
2024-09-18 22:47:24 +0800

    number cast func

--- pylox/interpreter/bulitin.py
+++ pylox/interpreter/bulitin.py
@@ -2,7 +2,17 @@ from abc import ABC, abstractmethod
 
 from rusty_utils import Ok
 
-from pylox.interpreter.error import LoxRuntimeResult
+from pylox.interpreter.error import LoxRuntimeError, LoxRuntimeResult
+
+class Return(LoxRuntimeError):
+    def __init__(self, return_value: object) -> None:
+        self. return_value = return_value
+
+    def __repr__ (self) -> str :
+        return f"Return({self.return_value})"
+
+    def __str__(self) -> str:
+        return self.__repr__()
 
 
 class LoxCallable(ABC):
@@ -19,7 +29,7 @@ class LoxCallable(ABC):
         pass
 
 
-class __Time_Impl(LoxCallable):
+class TimeImpl(LoxCallable):
     def arity(self) -> int:
         return 0
 
@@ -31,6 +41,29 @@ class __Time_Impl(LoxCallable):
         return "<native fn time>"
 
 
+class InputImpl(LoxCallable):
+    def arity(self) -> int:
+        return 1
+
+    def call(self, args: list[object]) -> LoxRuntimeResult[object]:
+        return Ok(input(args[0]))
+
+    def __repr__(self) -> str:
+        return "<native fn input>"
+
+class CastToNumberImpl(LoxCallable):
+    def arity(self) -> int:
+        return 1
+
+    def call(self, args: list[object]) -> LoxRuntimeResult[object]:
+        # force cast
+        return Ok(float(args[0])) # type: ignore
+
+    def __repr__(self) -> str:
+        return "<native fn number>"
+
 Builtin = {
-    "time": __Time_Impl()
+    "time": TimeImpl(),
+    "input": InputImpl(),
+    "number" : CastToNumberImpl(),
 }
